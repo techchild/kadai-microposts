ba4fbbe6393b17e7b27d72aee459b565595f0475
tech.child <wishborn2004+1@gmail.com>
2020-04-17 10:57:56 +0000

    timeline

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,8 +1,8 @@
 class ToppagesController < ApplicationController
   def index
-    if logged_in?
+    if logged_in? # ログイン済みか？であるならば、トップページに「投稿フォームを表示したい」←「form_withの1th引数modelに指定するオブジェクトが必要」
       @micropost = current_user.microposts.build  # form_with 用
-      @microposts = current_user.microposts.order(id: :desc).page(params[:page])
+      @microposts = current_user.feed_microposts.order(id: :desc).page(params[:page])
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'relationships/create'
+  get 'relationships/destroy'
   root to: 'toppages#index'
   
   get 'login', to: 'sessions#new'
@@ -7,7 +9,13 @@ Rails.application.routes.draw do
   
 
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
+  resources :users, only: [:index, :show, :new, :create] do
+    member do
+      get :followings
+      get :followers
+    end
+  end
   
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
